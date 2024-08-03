750a6f00fb410ec1ab7097d43c9990215eaf980f
raswanson01 <raswanson01@gmail.com>
2024-08-03 15:06:19 -0500

    Initial commit

---
+++ src/components/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let text;
+</script>
+
+<div class="
+    rounded-lg
+    shadow-md 
+    bg-blue-500 
+    text-white 
+    px-4 
+    py-2
+    m-5
+    cursor-pointer
+    hover:bg-blue-600
+     active:bg-blue-700 
+     transition-colors 
+     duration-150"
+>
+    <h3>{text}</h3>
+</div>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    let images = [];
+</script>
+
+<div class="container">
+    <h1>Tracker Time</h1>
+    <button>
+        <a href="/">Return to start</a>
+    </button>
+
+</div>
