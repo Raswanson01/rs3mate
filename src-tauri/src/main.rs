84d22d3df848a813ca6b9e26ce2200de2ea399ad
raswanson01 <raswanson01@gmail.com>
2025-03-12 06:30:55 -0500

    Adding initial sql migrations

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,22 +11,71 @@ fn greet(name: &str) -> String {
 
 fn main() {
 
-    // let migrations = vec![
-    //     // Define your migrations here
-    //     Migration {
-    //         version: 1,
-    //         description: "create_initial_tables",
-    //         sql: "
-    //         CREATE TABLE rotations (id INTEGER PRIMARY KEY, name TEXT);
-    //         CREATE TABLE abilities (id INTEGER PRIMARY KEY AUTO INCREMENT);
-    //         ",
-    //         kind: MigrationKind::Up,
-    //     }
-    // ];
+    let migrations = vec![
+        // Define your migrations here
+        Migration {
+            version: 1,
+            description: "create_initial_tables",
+            sql: "
+            CREATE TABLE rotations
+            (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                style TEXT
+            );
+            CREATE TABLE abilities 
+            (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                cooldown NUMERIC NOT NULL,
+                image TEXT NOT NULL
+            );
+            CREATE TABLE bars
+            (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            );
+            CREATE TABLE bars_abilities
+            (
+                id INTEGER NOT NULL PRIMARY KEY,
+                abilityId INTEGER NOT NULL,
+                barId INTEGER NOT NULL
+            );
+            CREATE TABLE rotations_abilities 
+            (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                abilityId INTEGER NOT NULL,
+                rotationId INTEGER NOT NULL,
+                orderNum INTEGER NOT NULL,
+                CONSTRAINT rotations_abilities_abilities_FK FOREIGN KEY (abilityId) REFERENCES abilities(id),
+                CONSTRAINT rotations_abilities_rotations_FK FOREIGN KEY (rotationId) REFERENCES rotations(id)
+            );
+            CREATE TABLE barConfigs 
+            (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            );
+            CREATE TABLE barConfigs_keybinds 
+            (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                barConfigId INTEGER NOT NULL,
+                keybind INTEGER,
+                position INTEGER NOT NULL
+            );
+            ",
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: "create_test_data",
+            sql: "INSERT INTO abilities VALUES (1, \"Greater Concentrated Blast\", 3.6, \"gconc.png\");",
+            kind: MigrationKind::Up
+        }
+    ];
     tauri::Builder::default()
-        // .plugin(tauri_plugin_sql::Builder::default()
-        //     .add_migrations("sqlite:rs3_mate_db.db", migrations)
-        //     .build())
+        .plugin(tauri_plugin_sql::Builder::default()
+            .add_migrations("sqlite:rs3_mate_db.db", migrations)
+            .build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
